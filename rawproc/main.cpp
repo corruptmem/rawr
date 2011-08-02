cbb0d8347d1998c6ee8a2ce57cc7c591df591232
Cameron Harris <cameron@cameronharris.org>
2011-08-02 18:53:06 +0100

    Initial Commit

---
+++ rawproc/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  rawproc
+//
+//  Created by Cameron Harris on 02/08/2011.
+//  Copyright 2011 Altis Partners. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
