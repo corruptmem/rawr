28b0518ad6ca3ce5da45762e32cc331adc81b94e
Cameron Harris <cameron@cameronharris.org>
2011-08-02 20:46:22 +0100

    Now i know where the raw data is!

--- rawproc/main.cpp
+++ rawproc/main.cpp
@@ -7,12 +7,137 @@
 //
 
 #include <iostream>
+#include <fstream>
+
+struct Cr2Header {
+    uint16_t byte_order;
+    uint16_t magic_word;
+    uint32_t tiff_offset;
+    uint16_t cr2_magic_word;
+    uint8_t cr2_major_ver;
+    uint8_t cr2_minor_ver;
+    uint32_t raw_ifd_offset;
+    
+public:
+    bool is_valid_header() {
+        if(byte_order!= 0x4949) 
+            return false;
+        
+        if(magic_word!= 0x002a)
+            return false;
+        
+        if(tiff_offset != 0x10)
+            return false;
+        
+        if(cr2_magic_word != 0x5243)
+            return false;
+        
+        if(cr2_major_ver!=0x2)
+            return false;
+        
+        if(cr2_minor_ver != 0x0)
+            return false;
+        
+        return true;
+    }
+};
+
+struct Cr2IfdEntry {
+    uint16_t tag_id;
+    uint16_t tag_type;
+    uint32_t number_of_val;
+    uint32_t val;
+};
+
+struct Cr2IfdHeader {
+    uint16_t entries_count;
+};
+
+struct Cr2IfdFooter {
+    uint32_t next_ifd;
+};
+
+struct Cr2Slices {
+    uint16_t num_first_strips;
+    uint16_t first_strip_px;
+    uint16_t last_strip_px;
+};
+
+class Cr2Parser {
+private:
+    const char* _path;
+    
+public:
+    Cr2Parser(const char* path) {
+        _path = path;
+    }
+    
+    void Parse() {
+        std::ifstream file(_path);
+        
+        if(!file.is_open()) {
+            throw "Error: Could not open file!";
+        }
+        
+        Cr2Header header;
+        file.read((char*)&header, sizeof(Cr2Header));
+        
+        if(!header.is_valid_header()){
+            throw "Error: Invalid header!";
+        }
+        
+        file.seekg(header.raw_ifd_offset, std::ios_base::beg);
+        
+        Cr2IfdHeader ifd_header;
+        file.read((char*)&ifd_header, sizeof(Cr2IfdHeader));
+        
+        uint32_t strip_offset;
+        uint32_t strip_byte_counts;
+        uint32_t slice_info_offset;
+        
+        for(int i = 0; i<ifd_header.entries_count; i++) {
+            Cr2IfdEntry ifd_entry;
+            file.read((char*)&ifd_entry, sizeof(Cr2IfdEntry));
+            
+            switch(ifd_entry.tag_id) {
+                case 0x103:
+                    if(ifd_entry.val!=6) throw "Err: Didn't understand compression type 6";
+                    break;
+                case 0x111:
+                    strip_offset = ifd_entry.val;
+                    break;
+                case 0x117:
+                    strip_byte_counts = ifd_entry.val;
+                    break;
+                case 0xc640:
+                    slice_info_offset = ifd_entry.val;
+            }
+        }
+        
+        Cr2IfdFooter ifd_footer;
+        file.read((char*)&ifd_footer, sizeof(Cr2IfdFooter));
+        
+        Cr2Slices slices;
+        file.seekg(slice_info_offset, std::ios_base::beg);
+        file.read((char*)&slices, sizeof(Cr2Slices));
+        
+        char* raw_buf = new char[strip_byte_counts];
+        file.seekg(strip_offset, std::ios_base::beg);
+        file.read(raw_buf, strip_byte_counts);
+        
+        std::cout << strip_byte_counts << std::endl;
+        
+        std::cout << "Done" << std::endl;
+    }
+};
 
 int main (int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    const char* file_path = "/Users/cameron/camraw/test.cr2";
+    Cr2Parser parser(file_path);
+    parser.Parse();
+    
+    
     return 0;
 }
 
